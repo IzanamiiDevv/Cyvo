2857002120e02cde4f4a85a5edc56dde2a1e7523
IzanamiiDevv <rafaeloli596@gmail.com>
2025-04-07 18:43:34 +0800

    Add Script

--- cyvo.py
+++ cyvo.py
@@ -14,22 +14,34 @@ print("""
 Creator: IzanamiiDevv.
 """)
 
-def Run_Connection(ip: str) -> None:
+def ControllLoop(ip: str) -> None:
     while True:
         prompt = input(f"""
 ┌──({ip}㉿Cyvo)-[~]
 └─$ """)
+        
         if prompt.strip().lower() == "exit":
             break
-        else:
-            print(f"Command '{prompt}' sent to {ip} (simulation)")
 
-ip: str = input("Enter the target IP: ")
+def MainLoop(ip: str) -> None:
+    while True:
+        prompt: str = input("[+]: ")
+
+        if prompt.strip().lower() == "exit":
+            break
+
+
+
 try:
+    ip: str = input("Enter the target IP: ")
+    print(f"Connecting to {ip}")
     response = requests.get(f"http://{ip}:8080", timeout=5)
     if response.status_code == 200:
-        Run_Connection(ip)
+        print("Connection Succeed")
+        MainLoop(ip)
     else:
         print(f"Connection failed with status code: {response.status_code}")
 except requests.RequestException:
     print("Execution Failed")
+except KeyboardInterrupt:
+    print("\nProcess Cancelled")
